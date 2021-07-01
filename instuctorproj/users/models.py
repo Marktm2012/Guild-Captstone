0d11268a6f065ce4adb7cb8381e1af89bbc7e4a1
Mark <meherin.mark@gmail.com>
2021-06-30 17:04:16 -0700

    User models and registration page updated

--- instuctorproj/users/models.py
+++ instuctorproj/users/models.py
@@ -2,16 +2,21 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
+
 class User(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
     birthday = models.DateField(default=timezone.now)
     email = models.EmailField()
     bio = models.TextField()
     location = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=11)
+    # Boolean to decifer between student and teacher
     is_instructor = models.BooleanField(default=False)
     # enrollments to be added once instructapp models are created
+    enrollments = models.ManyToManyField('instructapp.Course', related_name='course')
+
 
     def __str__(self):
         return self.last_name + ', ' + self.first_name
--- instuctorproj/users/views.py
+++ instuctorproj/users/views.py
@@ -1,5 +1,34 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import User
+from datetime import datetime
 
 def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        if 'is_instructor' in request.POST:
+            is_instructor = True
+        else:
+            is_instructor = False
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        location = request.POST['location']
+        birthday = request.POST['birthday']
+        birthday = datetime.strptime(birthday, '%Y-%m-%d')
+        bio = request.POST['bio']
+        # ----------------------------------
+        user = User.objects.create_user(username,email,password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.is_instructor = is_instructor
+        user.phone_number = phone_number
+        user.birthday = birthday
+        user.location = location
+        user.bio = bio
+        user.save()
     return render(request, ('users/register.html'))
+
+ 
