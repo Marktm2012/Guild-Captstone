ee238acc3885ec164f90e1bc3350c1f0dbe80a26
Mark <meherin.mark@gmail.com>
2021-07-14 15:58:05 -0700

    settings edit

--- instuctorproj/instuctorproj/settings.py
+++ instuctorproj/instuctorproj/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_on_heroku
-django_on_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +136,4 @@ LOGIN_URL = "/users/register/"
 MEDIA_URL = "/uploaded_files/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
+django_on_heroku.settings(locals())
